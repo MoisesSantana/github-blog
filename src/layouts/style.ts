e034d8d9391bc6aa4666135cbbe2bdd4d06880e4
Moisés Santana <moisaant@gmail.com>
2023-08-14 21:59:15 -0300

    feat: layout prototyping

---
+++ src/layouts/style.ts
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+export const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  max-width: 1280px;
+  margin: 0 auto;
+`;
